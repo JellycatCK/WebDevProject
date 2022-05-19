cc90f7f2f592a3039116fcd7ff44edfb53bb501e
JellycatCK <103029348+JellycatCK@users.noreply.github.com>
2022-05-19 16:37:09 +0100

    Added styles to the 8 region pages
    - Added nav bars and footers to the 8 region pages
    - Formatted the recipes lists using bootstrap

--- recipeRetrieval.js
+++ recipeRetrieval.js
@@ -1,7 +1,75 @@
+// Function to display a list of recipes for the given region
+function showRecipesByRegion(inputRegion) {
+
+    // Reads XML data 
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(xml, "text/xml");
+
+    var divRecipes = document.getElementById('recipes');      // THE PARENT DIV.
+    var recipes = xmlDoc.getElementsByTagName('recipe');       // THE XML TAG NAME.
+
+    for (var i = 0; i < recipes.length; i++) {
+
+        //Retrieve region for this recipe
+        regionValue = recipes[i].getElementsByTagName("region")[0].childNodes[0].nodeValue;
+
+        // Only display this recipe if it is matches the given region
+        if(regionValue == inputRegion || inputRegion == "ALL"){
+
+            // Create 'a' tag for each recipe
+            var aRecipe = document.createElement('a');
+            aRecipe.href = "recipe.html";
+            aRecipe.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
+
+            // Display picture for each recipe
+            var imgRecipe = document.createElement('img');
+            imgRecipe.src = recipes[i].getElementsByTagName("pictureURL")[0].childNodes[0].nodeValue;
+            imgRecipe.alt ="Pic 1";
+            imgRecipe.width = "64";
+            imgRecipe.height = "64";
+            aRecipe.appendChild(imgRecipe);
+
+            // Display recipe name
+            var divRecipeDetails = document.createElement("div");
+            divRecipeDetails.class = "d-flex gap-2 w-100 justify-content-between";
+            aRecipe.appendChild(divRecipeDetails);
+            
+            var divNameRecipeDetails = document.createElement("div");
+            divRecipeDetails.appendChild(divNameRecipeDetails);
+
+            var recipeName = document.createElement("h6");
+            recipeName.class = "mb-0";
+            recipeName.innerText = recipes[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+            divNameRecipeDetails.appendChild(recipeName);
+
+            // Display recipe servings
+            var recipeServings = document.createElement("small");
+            recipeServings.class = "opacity-50";
+            recipeServings.innerText = "Serves " + recipes[i].getElementsByTagName("servings")[0].childNodes[0].nodeValue;
+            divRecipeDetails.appendChild(recipeServings);
+        
+            // Append to list
+            divRecipes.appendChild(aRecipe);
+        }
+     }
+
+     function showAllRecipes() {
+         showRecipesByRegion("ALL");
+     }
+
+    // function populateRecipePage(recipe)
+    // {
+    //     if(document.readyState === 'complete'){
+    //         window.location = "recipe.html";
+    //         document.getElementById("recipeName").innerText = recipes[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    //     }
+    // }
+} 
+
 var xml =
 "<recipes>" +
     "<recipe>" +
-        "<name>curry1</name>" +
+        "<name>Curry 1</name>" +
         "<pictureURL>img/curry.jpg</pictureURL>" +
         "<region>south asia</region>" +
         "<servings>4</servings>" +
@@ -18,7 +86,7 @@ var xml =
     "</recipe>" +
 
     "<recipe>" +
-        "<name>curry2</name>" +
+        "<name>Curry 2</name>" +
         "<pictureURL>img/curry.jpg</pictureURL>" +
         "<region>south asia</region>" +
         "<servings>4</servings>" +
@@ -58,76 +126,3 @@ var xml =
     "<method>make it</method>" +
     "</recipe>" +
 "</recipes>";
-
-function showAllRecipes() {
-
-    // Reads XML data 
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(xml, "text/xml");
-
-    var divRecipes = document.getElementById('recipes');      // THE PARENT DIV.
-    var recipes = xmlDoc.getElementsByTagName('recipe');       // THE XML TAG NAME.
-
-    for (var i = 0; i < recipes.length; i++) {
-
-        // CREATE CHILD DIVS INSIDE THE PARENT DIV.
-        var divLeft = document.createElement('div');
-        divLeft.className = 'col1';
-        divLeft.innerHTML = recipes[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-
-        var divRight = document.createElement('div');
-        divRight.className = 'col2';
-        divRight.innerHTML = recipes[i].getElementsByTagName("region")[0].childNodes[0].nodeValue;
-
-        // ADD THE CHILD TO THE PARENT DIV.
-        divRecipes.appendChild(divLeft);
-        divRecipes.appendChild(divRight);
-    }
-};
-
-function showRecipesByRegion(inputRegion) {
-
-    // Reads XML data 
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(xml, "text/xml");
-
-    var divRecipes = document.getElementById('recipes');      // THE PARENT DIV.
-    var recipes = xmlDoc.getElementsByTagName('recipe');       // THE XML TAG NAME.
-
-    for (var i = 0; i < recipes.length; i++) {
-
-        //Retrieve region for this recipe
-        regionValue = recipes[i].getElementsByTagName("region")[0].childNodes[0].nodeValue;
-
-        // Only display this recipe if it is matches the given region
-        if(regionValue == inputRegion){
-            // CREATE CHILD DIVS INSIDE THE PARENT DIV.
-            var divLeft = document.createElement('button');
-            divLeft.className = 'col1';
-            divLeft.innerHTML = recipes[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-            // divLeft.addEventListener('click', populateRecipePage(recipes[i]), false);
-
-            divLeft.addEventListener('click', function (event){
-                populateRecipePage(recipes[i])
-                                          });
-
-            var divRight = document.createElement('img');
-            divRight.className = 'col2';
-            divRight.src = recipes[i].getElementsByTagName("pictureURL")[0].childNodes[0].nodeValue;
-            divRight.alt ="Pic 1";
-            divRight.width = "100";
-
-            // ADD THE CHILD TO THE PARENT DIV.
-            divRecipes.appendChild(divLeft);
-            divRecipes.appendChild(divRight);
-        }
-    }
-
-    function populateRecipePage(recipe)
-    {
-        if(document.readyState === 'complete'){
-            window.location = "recipe.html";
-            document.getElementById("recipeName").innerText = recipes[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-        }
-    }
-} 
